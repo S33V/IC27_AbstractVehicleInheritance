a1a3b95663dc56c5dec72d2e31e2ea523cddc7c7
Badajoz <sbadajoz@student.cccd.edu>
2016-12-09 12:06:37 -0800

    Compete Boat.java

--- src/edu/orangecoastcollege/sbadajoz/ic27/Boat.java
+++ src/edu/orangecoastcollege/sbadajoz/ic27/Boat.java
@@ -6,6 +6,50 @@ December 9, 2016
 
 IC27
 */
-public class Boat {
+public class Boat extends Vehicle {
+	private int mNumCabins;
+	
+	public  Boat(String manufacturer, int year, int numberOfCabins) {
+		mManufacturer = manufacturer;
+		mYear = year;
+		mNumCabins = numberOfCabins;
+	}
 
+	public int getNumberofCabins() {
+		return mNumCabins;
+	}
+
+	public void setNumberOfCabins(int numberOfCabins) {
+		this.mNumCabins = numberOfCabins;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + mNumCabins;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Boat other = (Boat) obj;
+		if (mNumCabins != other.mNumCabins)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[Boat: " + mManufacturer + ", " + mYear
+				+ ", " + mNumCabins + " cabin" + ((mNumCabins > 1 || mNumCabins == 0)?"s":"") + "]";
+	}
+	
+	
 }
